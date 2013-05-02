4db5b2e40a93235d372d315177d088a8084b8cbb
Caroline E. Willis <cewillismail@gmail.com>
2013-05-02 18:42:52 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobileframezero::Application.config.secret_token = 'cebfb0e536c46a0a4f3f35688fbb9070e25c874b88daafd1d4388783ef5907bb2e4f7619426caa2ccb962fe34b22c2fad2f16aef80254d51f3c91e3ddfc94dbf'
